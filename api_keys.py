7aa721255bcc5a6b3d906d2b046e782c0c66751b
Raj Sisodia <42306046+rajkamal85@users.noreply.github.com>
2020-03-12 23:35:05 -0400

    Delete api_keys.py

--- api_keys.py
+++
@@ -1,7 +0,0 @@
-# OpenWeatherMap API Key
-weather_api_key = "bc700cd31805870dbc2b5c26d744332b"
-
-# Google API Key
-g_key = "AIzaSyCZmZE2tJWkcqIdokA4-V3vv0vVJD3b4vg"
-
-p_key = "AIzaSyBMZziFrdj-SOZ4IBWHsmeKpYRuJJdAMjA"
